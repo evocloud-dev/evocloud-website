7356feb305943507c5d66abb034e57ce7a4666eb
geanttechnology <geanttechnology@gmail.com>
2025-09-22 02:03:09 -0400

    Added css styles for various pages

--- app/[locale]/evocluster-apps/page.tsx
+++ app/[locale]/evocluster-apps/page.tsx
@@ -1,20 +1,20 @@
 import Hero from "@/components/specific/evocluster-apps/Hero";
 import Evocluster from "@/components/specific/evocluster-apps/EvoCluster";
+import Signup from "@/components/specific/evocluster-apps/Signup";
 // import Features from "@/components/specific/evocloud/Features";
 // import Why from "@/components/specific/evocloud/Why";
 // import Learn from "@/components/specific/evocloud/Learn";
-// import Signup from "@/components/specific/evocloud/Signup";
 
 export default function Page() {
   return (
     <div className="max-w-full overflow-x-hidden">
       <Hero />
       <Evocluster />
+      <Signup />
       {/* <Evocloud/> */}
       {/* <Features/> */}
       {/* <Why/> */}
       {/* <Learn/> */}
-      {/* <Signup/> */}
     </div>
   );
 }
--- components/specific/evocloud/Evocloud.tsx
+++ components/specific/evocloud/Evocloud.tsx
@@ -9,12 +9,11 @@ import P1 from "@/components/ui/paragraphs/P1";
 export default function Evocloud() {
   return (
     <>
-      <div className="flex items-center justify-center bg-linear-to-b from-accent to-primary px-4 py-6 md:px-4 md:py-16">
+      <div className="flex items-center justify-center bg-gray-100 text-primary-foreground px-4 py-6 md:px-4 md:py-16">
         <div className="max-w-screen-xl w-full mx-auto space-y-10">
-          <H1 className="text-center">Why did we build EvoCloud?</H1>
+          <H1 className="text-center text-primary mb-4">Why did we build EvoCloud?</H1>
         </div>
       </div>
     </>
   );
 }
-
